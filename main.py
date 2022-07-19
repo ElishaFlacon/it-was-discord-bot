54fe8d32a69eb0e20db4528d06277cb1c7606fdf
Elisha <83610362+ElishaFlacon@users.noreply.github.com>
2022-07-19 23:44:10 +0500

    upd 2.0
    Гигантское обновление кода и добавление новых функций, а точнее ответ на слово Быдло, а так же ответ на слово было даже внутри предложений

--- main.py
+++ main.py
@@ -6,41 +6,72 @@ import json
 import requests
 
 
+# Инициализируем бота
+client = discord.Client()
 print('Start Working!')
 
-bot = commands.Bot(command_prefix=settings['prefix'])
 
-
+# Функция выбора было или не было
 def it_was_or_not():
-    rng = random.randrange(1, 10000)
-    if rng > 5000:
+    rng = random.randrange(1, 1000)
+    if rng > 500:
         return 'Было!'
-    elif rng == 9827:
+    elif rng == 501:
         return 'Эмм... я хз, если честно :P'
     else:
         return 'Не Было!'
 
 
+# Функция создания рандомного изображения
 def rng_img():
     image_data = ['img/cat', 'img/fox', 'animu/wink']
     image_weight = [50, 30, 20]
-
     img = random.choices(image_data, image_weight)[0]
-
     return img
 
 
-@bot.command()
-async def о(ctx):
-    author = ctx.message.author
-    response = requests.get(f'https://some-random-api.ml/{rng_img()}')
-    json_data = json.loads(response.text)
+# Функция создания интеграции?! ну этого embed
+def create_embded(message, proop):
+    if proop == True:
+        response = requests.get(f'https://some-random-api.ml/img/dog')
+        json_data = json.loads(response.text)
+        #  Создаем интеграцию
+        embed = discord.Embed(
+            color=0xff9900, title=f'{message.author.name}, САМ БЫДЛО ПЫХ ПЫХ!!!')
+        embed.set_image(url=json_data['link'])
+    else:
+        # Через жопу генерируем ссылку на картинку
+        response = requests.get(f'https://some-random-api.ml/{rng_img()}')
+        json_data = json.loads(response.text)
+        #  Создаем интеграцию
+        embed = discord.Embed(
+            color=0xff9900, title=f'{message.author.name}, {it_was_or_not()}')
+        embed.set_image(url=json_data['link'])
 
-    embed = discord.Embed(
-        color=0xff9900, title=f'{author.name}, {it_was_or_not()}')
-    embed.set_image(url=json_data['link'])
-
-    await ctx.send(embed=embed)
+    return embed
 
 
-bot.run(settings['token'])
+#  Декоратор с функцией прослушивания сообщений
+@client.event
+async def on_message(message):
+    msg = message.content.lower()
+    if message.author == client.user:
+        return
+    src_active = False
+    if msg.startswith('было'):
+        src_active = True
+        await message.channel.send(embed=create_embded(message, False))
+    if msg.startswith('быдло'):
+        src_active = True
+        await message.channel.send(embed=create_embded(message, True))
+    if msg.startswith('') and src_active == False:
+        author_msg = msg.split()
+        for i in author_msg:
+            if i == 'было':
+                await message.channel.send(embed=create_embded(message, False))
+            if i == 'быдло':
+                await message.channel.send(embed=create_embded(message, True))
+
+
+#  Инициализируем бота
+client.run(settings['token'])
