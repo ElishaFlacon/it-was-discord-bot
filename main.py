12a4f9425d50a1601f6ab629ce5e0719a51362b0
Elisha <83610362+ElishaFlacon@users.noreply.github.com>
2022-07-22 16:47:58 +0500

    upd 2.2 Add jokes
    Добавлена команда !анекдот, после ее вызова бот отправляет случайный анекдот

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from config import settings
 import random
 import json
 import requests
+from bs4 import BeautifulSoup
 
 
 # Инициализируем бота
 client = discord.Client()
-print('Start Working!')
 
 
 # Функция выбора было или не было
@@ -34,7 +34,7 @@ def create_embded(message, proop):
     if proop == True:
         response = requests.get(f'https://some-random-api.ml/img/dog')
         json_data = json.loads(response.text)
-        #  Создаем интеграцию
+        # Создаем интеграцию
         embed = discord.Embed(
             color=0xff9900, title=f'{message.author.name}, САМ БЫДЛО ПЫХ ПЫХ!!!')
         embed.set_image(url=json_data['link'])
@@ -42,32 +42,52 @@ def create_embded(message, proop):
         # Через жопу генерируем ссылку на картинку
         response = requests.get(f'https://some-random-api.ml/{rng_img()}')
         json_data = json.loads(response.text)
-        #  Создаем интеграцию
+        # Создаем интеграцию
         embed = discord.Embed(
             color=0xff9900, title=f'{message.author.name}, {it_was_or_not()}')
         embed.set_image(url=json_data['link'])
     return embed
 
 
-#  Декоратор с функцией прослушивания сообщений
-@client.event
+# Функция генерации анекдота
+def create_joke():
+    jokes_list = []
+    url_joke = f'https://4tob.ru/anekdots/tag/black/page{int(random.uniform(1, 4))}'
+    joke_req = requests.get(url_joke)
+    soup = BeautifulSoup(joke_req.text, 'html.parser')
+    jokes = soup.find_all('div', class_='text')
+    for joke in jokes:
+        jokes_list.append(joke.text)
+    return str(random.choice(jokes_list))
+
+
+# Декоратор с функцией прослушивания сообщений
+@ client.event
 async def on_message(message):
     msg = message.content.lower()
     if message.author == client.user:
         return
-    #  С помощью метода финд находим нужное слово
-    #  Даже в такой строчке - 'sdЫ2@4аы2быЛоффООФЫВ2392'
-    #  Так как если финд ничего не нашел он возвращает -1, то мы ставим условие >= 0
+
+    if msg.startswith('!анекдот'):
+        print(
+            f'{message.author.name} написал: \"\"{message.content}\"\"')
+        await message.channel.send(create_joke())
+
+    # С помощью метода финд находим нужное слово
+    # Даже в такой строчке - 'sdЫ2@4аы2быЛоффООФЫВ2392'
+    # Так как если финд ничего не нашел он возвращает -1, то мы ставим условие >= 0
     if msg.startswith(''):
         if msg.find('было') >= 0:
             print(
-                f'{message.author.name} написал: \"\"{message.content}\"\" И получил ответ, который я пока не знаю :P')
+                f'{message.author.name} написал: \"\"{message.content}\"\"')
             await message.channel.send(embed=create_embded(message, False))
         if msg.find('быдло') >= 0:
             print(
-                f'{message.author.name} написал: \"\"{message.content}\"\" И ОН БЫДЛО!!!')
+                f'{message.author.name} написал: \"\"{message.content}\"\"')
             await message.channel.send(embed=create_embded(message, True))
 
 
-#  Инициализируем бота
+# Работаем
+print('Start Working!')
+# Инициализируем бота
 client.run(settings['token'])
